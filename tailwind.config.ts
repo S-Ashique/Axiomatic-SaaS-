28fcc792c8803ae9e3f42c8ff17b522fc4d2da30
Ashique <aashiquee13@gmail.com>
2024-09-05 10:15:33 -0700

    light Sass landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+          lg: "80px",
+        },
       },
     },
   },
